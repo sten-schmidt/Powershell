d287e3b25fbaae7a30f1247a0f64f7cfdc7993c7
ST <email@sten-schmidt.net>
2017-09-23 17:59:01 +0200

    Example: Send email using SMTP

---
+++ SMTP/Send-Email.ps1
@@ -0,0 +1,14 @@
+﻿#
+# Send email using SMTP
+#
+
+$smtp = "mystmpserver.example.local"
+$user = "mystmpuser"
+$pass = "mysmtppassword" | ConvertTo-SecureString -asPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $pass)
+
+Send-MailMessage -From sender@example.local `
+    -Subject "Test Email from Powershell" -To recipient@example.local `
+    -Body "Hallo, this is a Test" -Port 587 -SmtpServer $smtp `
+    -Credential $cred -UseSsl
+    
