5693ea9de89c4e2f933fe7eccba9be6f4062ba85
ST <email@sten-schmidt.net>
2017-09-23 17:29:17 +0200

    Example: Call StoredProcedure with ExecuteReader

---
+++ SqlServer/Call-StoredProcedure-ExecuteReader.ps1
@@ -0,0 +1,24 @@
+﻿#
+# Call StoredProcedure with ExecuteReader
+#
+
+[string]$connectionString = "Server=LOCALHOST;Database=DevTest;Integrated Security=True"
+
+$con = New-Object System.Data.SqlClient.SqlConnection -ArgumentList @($connectionString)
+$con.Open()
+
+#Table-Result
+$cmd = $con.CreateCommand()
+$cmd.CommandType=[System.Data.CommandType]::StoredProcedure
+$cmd.CommandText = "spGetTestData01"
+
+$reader = $cmd.ExecuteReader()
+while ($reader.Read() -eq $true)
+{
+    Write-Host $reader['ID'] -> $reader['Name']
+}
+
+$reader.Close()
+$con.Close()
+ 
+ 
