789fe2090dd8ab736cd886ac0bfec3282ce460d5
ST <email@sten-schmidt.net>
2017-09-23 18:50:10 +0200

    Example: List all Databases using SMO

---
+++ SqlServer/ListAll-Databases.ps1
@@ -0,0 +1,16 @@
+﻿#
+# List all Databases using SMO
+#
+
+Import-Module SQLPS
+
+$instanceName = "LOCALHOST"
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($instanceName)
+[Microsoft.SqlServer.Management.Smo.DatabaseCollection]$dbList = $server.Databases
+
+"Databases found: " + $dbList.Count
+
+foreach ($db in $dbList) {
+    Write-Host $db.Name 
+}
+
