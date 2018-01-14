fa0f40af5114e0af9484106ed216987272ea22e1
ST <email@sten-schmidt.net>
2018-01-14 09:54:38 +0100

    refactoring to script with parameters

--- Sharepoint/Add-FileToSPDocLib.ps1
+++ Sharepoint/Add-FileToSPDocLib.ps1
@@ -1,27 +1,48 @@
 ﻿#
 # Upload a File to Sharepoint document library 
+# .\Add-FileToSPDocLib -SpWebUrl "http://sharepoint/websites/testweb" -DocLibName  “Doks” -FilePath “C:\Temp\Neues Textdokument.txt” -override $true
 #
+
+Param(
+    [Parameter(Mandatory=$true)][String]$SpWebUrl,
+    [Parameter(Mandatory=$true)][String]$DocLibName,
+    [Parameter(Mandatory=$true)][String]$FilePath,
+    [Parameter(Mandatory=$true)][Bool]$Override
+)
 
 Add-PsSnapin Microsoft.SharePoint.PowerShell
 
-function Add-FileToSPDocLib([String]$spWebUrl, [String]$docLibName, [String]$filePath, [Bool]$override) {
-
-    [Microsoft.SharePoint.SPWeb]$spWeb = Get-SPWeb $spWebUrl 
-    [Microsoft.SharePoint.SPFolder]$spFolder = $spWeb.GetFolder($docLibName) 
-    [Microsoft.SharePoint.SPFileCollection]$spFileCollection = $spFolder.Files
+try
+{
     
-    [IO.FileSystemInfo]$file = Get-ChildItem $filePath
-    [String]$fileName = $file.Name
+    function Add-FileToSPDocLib([String]$spWebUrl, [String]$docLibName, [String]$filePath, [Bool]$override) {
 
-    [IO.FileStream]$fs = $file.OpenRead()
+        [Microsoft.SharePoint.SPWeb]$spWeb = Get-SPWeb $spWebUrl 
+        [Microsoft.SharePoint.SPFolder]$spFolder = $spWeb.GetFolder($docLibName) 
+        [Microsoft.SharePoint.SPFileCollection]$spFileCollection = $spFolder.Files
+    
+        [IO.FileSystemInfo]$file = Get-ChildItem $filePath
+        [String]$fileName = $file.Name
 
-    $spFileCollection.Add($docLibName + "/" + $fileName, $fs, $override)  | Out-Null
+        [IO.FileStream]$fs = $file.OpenRead()
 
-    $fs.Dispose()
-    $file = $null
-    $spFileCollection = $null
-    $spFolder = $null
-    $spWeb.Dispose()	
+        $spFileCollection.Add($docLibName + "/" + $fileName, $fs, $override)  | Out-Null
+
+        $fs.Dispose()
+        $file = $null
+        $spFileCollection = $null
+        $spFolder = $null
+        $spWeb.Dispose()	
+    }
+
+    Add-FileToSPDocLib -spWebUrl $SpWebUrl -docLibName $DocLibName -filePath $FilePath -override $Override
+
 }
-
-Add-FileToSPDocLib -spWebUrl "http://sharepoint/websites/testweb" -docLibName  “Doks” -filePath “C:\Temp\Neues Textdokument.txt” -override $true
+catch [System.Exception]
+{
+    Write-Error -Message $Error[0]
+}
+finally
+{
+    
+}
