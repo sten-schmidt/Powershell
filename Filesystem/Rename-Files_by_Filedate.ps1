3a6dd50bcee7091b697863c785cf8edc18026c4f
Sten Schmidt <sten.schmidt@outlook.com>
2020-04-26 14:03:24 +0200

    + Filesystem/Rename-Files

---
+++ Filesystem/Rename-Files_by_Filedate.ps1
@@ -0,0 +1,11 @@
+﻿#
+# Rename-Files, add the changedate to the filename.
+#
+
+$baseDir = "C:\Temp\"
+$baseDirFilter = "*"
+Set-Location -Path $baseDir
+Get-Childitem -Path $baseDir -Filter $baseDirFilter | ForEach-Object {
+    $fileNameNew = $_.LastWriteTime.ToString("yyyy-MM-dd_HHmm") + "_" + $_.Name
+    Rename-Item -Path $_ -NewName $fileNameNew -WhatIf
+}
